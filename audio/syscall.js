3735f60040cb0fd1a9fc3dca36f0696907984d5c
Gabriel Delavald <gdelavald@gmail.com>
2015-10-31 17:36:28 -0200

    child process para tocar audio

--- audio/syscall.js
+++ audio/syscall.js
@@ -1,10 +1,9 @@
 var child = require('child_process');
 
-child.execFile('mpg321', ['audio/bell.mp3'], function(err, stdout, stderr) {
-	console.log(err);
-	setTimeout(killProcess, 3000);
-});
-
-var killProcess = function() {
-	child.exit();
+module.exports = {
+	play : function(filename) { 
+		child.exec('mpg321 '+filename, function(err, stdout, stderr) {
+		console.log(err);	
+		});
+	} 
 }
