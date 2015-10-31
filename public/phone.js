9a5bdbd6e0921850976b9614fb002905066a6a1d
Gabriel Delavald <gdelavald@gmail.com>
2015-10-31 18:47:22 -0200

    zzz

--- public/phone.js
+++ public/phone.js
@@ -3,6 +3,13 @@ var Gpio = require('onoff').Gpio,
 	arduino_14 = new Gpio(14, 'out'),
 	arduino_16 = new Gpio(16, 'out');
 
+var piblaster = require('pi-blaster.js');
+
+piblaster.setPwm(14, 1 ); // 100% brightness
+piblaster.setPwm(16, 1); //# 20% brightness
+
+
+
 arduino_4.watch(function(err, value) {
   console.log("input value is = "+value);
 });
