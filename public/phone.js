39e02c9ac6940f4503e41880eac571ba0452651d
cmdalbem <ninja.dalbem@gmail.com>
2015-11-01 03:38:07 -0200

    basic communication protocol with Arduino

--- public/phone.js
+++ public/phone.js
@@ -1,33 +1,59 @@
+var phoneAudio = require('../audio/syscall.js');
+
 var SerialPort = require("serialport").SerialPort;
 
 // var serialPort = new SerialPort("/dev/ttyACM0", {
-var serialPort = new SerialPort("/dev/cu.usbmodem1421", {
-	baudrate: 9600
-});
+var serialPort;
+var buffer = "";
 
-
-serialPort.on("open", function () {
-	console.log('open');
-
-	serialPort.on('data', function(data) {
-		console.log('data received: ' + data);
-
-		if (data && data.length>0) {
-			if (data=="P") {
-				console.log('PICKED UP');
-			}
-			else {
-				console.log("RATING = ", data);
-			}
-		}
-		
+try {
+	serialPort = new SerialPort("/dev/cu.usbmodem1411", {
+		baudrate: 9600
 	});
+} catch (e) {
+	console.error("Couldn't open SerialPort");
+}
 
-	// serialPort.write(new Buffer('4','ascii'), function(err, results) {
-	// 	console.log('err ' + err);
-	// 	console.log('results ' + results);
-	// }); 
-});
+if (serialPort) {
+	serialPort.on("open", function () {
+		console.log('open');
+
+		serialPort.on('data', function(data) {
+			// console.log(data);
+
+			var size = data.length;
+
+			for(var i=0; i<size; i++) {
+				var character = String.fromCharCode(data[i]);
+				if (character != ".") {
+					buffer += character;	
+				} else {
+					// console.log('finished receiving: ' + buffer);
+
+					if (buffer && buffer.length>0) {
+						if (buffer=="P") {
+							console.log('PICKED UP');
+							// phoneAudio.playMp3('audio/bell.mp3');
+						}
+						else if (buffer=="I") {
+							console.log("IDLE");
+						} else  if (buffer[0]=="d") {
+							console.log("RATING = ", buffer[1]);
+						}
+					}
+
+					buffer = "";
+				}
+			}
+			
+		});
+
+		// serialPort.write(new Buffer('4','ascii'), function(err, results) {
+		// 	console.log('err ' + err);
+		// 	console.log('results ' + results);
+		// }); 
+	});
+}
 
 module.exports = {
 	playQuote : function(data) {
